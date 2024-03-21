325675baa1d4fe8eee98d40fb0681065b9366faf
semuadmin <28569967+semuadmin@users.noreply.github.com>
2024-03-21 22:28:59 +0000

    update gnssapp example

--- examples/gnssapp.py
+++ examples/gnssapp.py
@@ -265,7 +265,7 @@ class GNSSSkeletonApp:
             cfg_data.append((f"CFG_MSGOUT_UBX_NAV_PVT_{port_type}", enable))
             cfg_data.append((f"CFG_MSGOUT_UBX_NAV_SAT_{port_type}", enable * 4))
             cfg_data.append((f"CFG_MSGOUT_UBX_NAV_DOP_{port_type}", enable * 4))
-            cfg_data.append((f"CFG_MSGOUT_UBX_RXM_RTCM_{port_type}", enable))
+            cfg_data.append((f"CFG_MSGOUT_UBX_RXM_COR_{port_type}", enable))
 
         msg = UBXMessage.config_set(layers, transaction, cfg_data)
         self.sendqueue.put((msg.serialize(), msg))
